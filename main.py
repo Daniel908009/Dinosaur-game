19a83840663936b8b7d4130b32c6be9792c3981a
Daniel Kriz <147802055+Daniel908009@users.noreply.github.com>
2024-07-31 15:37:30 +0200

    new cactus images, collision, outline of the settings
    also added a temporary image of a pterodactyl, and some small improvements all over the code

--- main.py
+++ main.py
@@ -1,10 +1,98 @@
 import pygame
 import random
-import time
+import tkinter
+import threading
 
     # functions
+# function to reset the game
 def reset():
-    pass
+    global player, score, player_change, cactuses, time
+    player = Dinosaur()
+    score = 0
+    player_change = 0
+    time = [0,0]
+    cactuses.empty()
+    cactus = Cactus()
+    cactuses.add(cactus)
+
+# function to update the timer
+def timer():
+    global time, game_over, settings
+    while running:
+        while game_over:
+            pass
+        while settings:
+            pass
+        time[1] += 1
+        if time[1] == 60:
+            time[0] += 1
+            time[1] = 0
+        pygame.time.wait(1000)
+
+# function to display the game over screen
+def game_over_screen():
+    global running, game_over
+    game_over = True
+    while game_over:
+        # Handling events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                game_over = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_r:
+                    game_over = False
+                    reset()
+        # background
+        win.fill((255, 255, 255))
+        # drawing a straight line to represent the ground
+        pygame.draw.line(win, (0, 0, 0), (0, 500), (800, 500), 5)
+        # drawing end message
+        text = font.render("Game Over! your score is "+str(score), True, (0, 0, 0))
+        win.blit(text, (WIDTH//2 - text.get_width()//2, HEIGHT//2 - text.get_height()//2))
+
+        # Updating the display
+        pygame.display.update()
+        # Frame rate
+        clock.tick(60)
+
+# function to apply the settings
+def apply_settings(window):
+    window.destroy()
+
+# function for the settings window
+def settings_window():
+    window = tkinter.Tk()
+    window.title("Settings")
+    window.geometry("300x300")
+    window.resizable(False, False)
+    #window.iconbitmap("icon.ico")
+    # setting up the main label
+    label = tkinter.Label(window, text="Settings", font=("Arial", 24))
+    label.pack()
+    # setting up a frame for the settings
+    frame = tkinter.Frame(window)
+    frame.pack()
+    # setting up the first setting
+    setting1 = tkinter.Label(frame, text="Setting 1", font=("Arial", 16))
+    setting1.grid(row=0, column=0)
+    setting1_entry = tkinter.Entry(frame, font=("Arial", 16))
+    setting1_entry.grid(row=0, column=1)
+    # setting up the second setting
+    setting2 = tkinter.Label(frame, text="Setting 2", font=("Arial", 16))
+    setting2.grid(row=1, column=0)
+    setting2_entry = tkinter.Entry(frame, font=("Arial", 16))
+    setting2_entry.grid(row=1, column=1)
+    # setting up the third setting
+    setting3 = tkinter.Label(frame, text="Setting 3", font=("Arial", 16))
+    setting3.grid(row=2, column=0)
+    setting3_entry = tkinter.Entry(frame, font=("Arial", 16))
+    setting3_entry.grid(row=2, column=1)
+
+    # setting up the apply button
+    apply_button = tkinter.Button(window, text="Apply", font=("Arial", 16), command= lambda: apply_settings(window))
+    apply_button.pack(side="bottom")
+    window.mainloop()
 
     # classes 
 class Dinosaur(pygame.sprite.Sprite):
@@ -17,7 +105,6 @@ class Dinosaur(pygame.sprite.Sprite):
     def draw(self, win):
         win.blit(self.image, (self.rect.x, self.rect.y))
     def move(self, change):
-        print(self.rect.y)
         # if the players y is close enough to the ground then he will be placed on the ground
         if self.rect.y > 390 and self.rect.y < 400:
             self.rect.y = 400
@@ -52,12 +139,17 @@ pygame.init()
 dinosaur = pygame.image.load('dinosaur1.png')
 resized_dinosaur = pygame.transform.scale(dinosaur, (100, 100))
 
-cactus_sizes = [70,75,80,85,90]
+cactus_sizes = [70,75,80]
 cactus1 = pygame.image.load('cactus1.png')
 cactus2 = pygame.image.load('cactus2.png')
 cactus3 = pygame.image.load('cactus3.png')
-cactuses_images = [cactus1, cactus2, cactus3]
-#resized_cactuses = [pygame.transform.scale(cactus1, (cactus_scale , cactus_scale )), pygame.transform.scale(cactus2, (cactus_scale , cactus_scale )), pygame.transform.scale(cactus3, (cactus_scale , cactus_scale ))]
+cactus4 = pygame.image.load('cactus4.png')
+cactus5 = pygame.image.load('cactus5.png')
+cactus6 = pygame.image.load('cactus6.png')
+cactuses_images = [cactus1, cactus2, cactus3, cactus4, cactus5, cactus6]
+
+pterodactyl = pygame.image.load('pterodactyl_temporary.png')
+resized_pterodactyl = pygame.transform.scale(pterodactyl, (100, 100))
 
 settings_button = pygame.image.load('settings.png')
 resized_settings_button = pygame.transform.scale(settings_button, (50, 50))
@@ -84,17 +176,27 @@ pygame.display.set_icon(resized_dinosaur)
 
 # Setting up the clock
 clock = pygame.time.Clock()
+# setting up a thread for the timer
+timer_thread = threading.Thread(target=timer)
+time = [0,0]
 
 # main loop
 running = True
+game_over = False
+settings = False
+day_night_cycle = False # I will use this later
 while running:
+
+    # starting the timer thread
+    if not timer_thread.is_alive():
+        timer_thread.start()
     # Handling events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE or event.key == pygame.K_UP or event.key == pygame.K_w:
-                player_change = -resized_dinosaur.get_height()*1.5
+                player_change = -resized_dinosaur.get_height()*1.8
             if event.key == pygame.K_DOWN or event.key == pygame.K_s:
                 player_change = resized_dinosaur.get_height()//2
             if event.key == pygame.K_r:
@@ -107,7 +209,8 @@ while running:
                 player_change = 0
         if event.type == pygame.MOUSEBUTTONDOWN:
             if 750 <= event.pos[0] <= 800 and 10 <= event.pos[1] <= 60:
-                print("Settings button clicked")
+                settings = True
+                settings_window()
     
     # applying changes in position
     player.move(player_change)
@@ -121,6 +224,9 @@ while running:
     win.blit(text, (10, 10))
     # drawing the settings button in the top right corner
     win.blit(resized_settings_button, (750, 10))
+    # drawing the time in the top middle of the screen
+    text = font.render(str(time[0])+"m "+str(time[1]) + "s", True, (0, 0, 0))
+    win.blit(text, (350, 10))
 
     # Drawing the player
     player.draw(win)
@@ -138,6 +244,13 @@ while running:
             # respawning the testing cactus
             cactus = Cactus()
             cactuses.add(cactus)
+    
+    # checking for collisions between the player and the cactuses
+    if pygame.sprite.spritecollide(player, cactuses, False):
+        game_over_screen()
+    
+    # if the code reaches this part then the settings window is closed
+    settings = False
 
     # Updating the display
     pygame.display.update()
