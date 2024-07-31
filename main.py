bc2fdc73939f269a52cb0c45a39c21a40218a29d
Daniel Kriz <147802055+Daniel908009@users.noreply.github.com>
2024-07-31 19:05:50 +0200

    added day night cycle and did some work on resizability
    also I never noticed until now that the draw functions in my classes are useless when using sprite groups, so I deleted them

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import threading
 # function to reset the game
 def reset():
     global player, score, player_change, enemies, time, cactus, pterodactyls, cycle, switch_cycle
+    cycle = "day"
     player = Dinosaur()
     score = 0
     player_change = 0
@@ -15,7 +16,6 @@ def reset():
     cactus = Cactus()
     enemies.add(cactus)
     pterodactyls.empty()
-    cycle = "day"
     switch_cycle = False
 
 # function to update the timer
@@ -53,25 +53,39 @@ def game_over_screen():
                 if event.key == pygame.K_r:
                     game_over = False
                     reset()
-        # background
-        win.fill((255, 255, 255))
-        # drawing a straight line to represent the ground
-        pygame.draw.line(win, (0, 0, 0), (0, 500), (800, 500), 5)
-        # drawing score in the top left corner
-        text = font.render("Score: "+str(score), True, (0, 0, 0))
-        win.blit(text, (10, 10))
-        # drawing the settings button in the top right corner
-        win.blit(resized_settings_button, (750, 10))
-        # drawing the time in the top middle of the screen
-        text = font.render(str(time[0])+"m "+str(time[1]) + "s", True, (0, 0, 0))
-        win.blit(text, (350, 10))
+        # what gets displayed depends on the cycle
+        if cycle == "day":
+            win.fill((255, 255, 255))
+            # drawing the ground
+            pygame.draw.line(win, (0, 0, 0), (0, 500), (800, 500), 5)
+            # drawing the score
+            text = font.render("Score: "+str(score), True, (0, 0, 0))
+            win.blit(text, (10, 10))
+            # drawing the game over text
+            text = font.render("Game Over", True, (0, 0, 0))
+            win.blit(text, (350, 250))
+            # drawing the timer
+            text = font.render(str(time[0])+"m "+str(time[1]) + "s", True, (0, 0, 0))
+            win.blit(text, (350, 10))
+        else:
+            win.fill((0, 0, 0))
+            # drawing the ground
+            pygame.draw.line(win, (255, 255, 255), (0, 500), (800, 500), 5)
+            # drawing the score
+            text = font.render("Score: "+str(score), True, (255, 255, 255))
+            win.blit(text, (10, 10))
+            # drawing the game over text
+            text = font.render("Game Over", True, (255, 255, 255))
+            win.blit(text, (350, 250))
+            # drawing the timer
+            text = font.render(str(time[0])+"m "+str(time[1]) + "s", True, (255, 255, 255))
+            win.blit(text, (350, 10))
+
         # drawing the player
         player.draw(win)
-        # drawing the cactuses
+        
+        # drawing the cactuses and the pterodactyls
         enemies.draw(win)
-        # drawing end message
-        text = font.render("Game Over! your score is "+str(score), True, (0, 0, 0))
-        win.blit(text, (WIDTH//2 - text.get_width()//2, HEIGHT//2 - text.get_height()//2))
 
         # Updating the display
         pygame.display.update()
@@ -79,8 +93,17 @@ def game_over_screen():
         clock.tick(60)
 
 # function to apply the settings
-def apply_settings(window):
+def apply_settings(window, setting1):
     window.destroy()
+    global day_night_cycle
+    if setting1:
+        day_night_cycle = True
+    else:
+        day_night_cycle = False
+
+    
+    # reseting the game to apply the changes
+    reset()
 
 # function for the settings window
 def settings_window():
@@ -103,7 +126,7 @@ def settings_window():
         e1.set("Day")
     else:
         e1.set("Night")
-    setting1_checkbox = tkinter.Checkbutton(frame, font=("Arial", 16), variable=e1, onvalue="Day", offvalue="Night")
+    setting1_checkbox = tkinter.Checkbutton(frame, font=("Arial", 16), variable=e1)
     setting1_checkbox.grid(row=0, column=1)
     # setting up the second setting
     setting2 = tkinter.Label(frame, text="Setting 2", font=("Arial", 16))
@@ -117,7 +140,7 @@ def settings_window():
     setting3_entry.grid(row=2, column=1)
 
     # setting up the apply button
-    apply_button = tkinter.Button(window, text="Apply", font=("Arial", 16), command= lambda: apply_settings(window))
+    apply_button = tkinter.Button(window, text="Apply", font=("Arial", 16), command= lambda: apply_settings(window, e1.get()))
     apply_button.pack(side="bottom")
     window.mainloop()
 
@@ -125,12 +148,19 @@ def settings_window():
 class Dinosaur(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = resized_dinosaur
-        self.rect = self.image.get_rect()
-        self.rect.x = 40
-        self.rect.y = 400
+        self.image_day = resized_dinosaur_day
+        self.image_night = resized_dinosaur_night
+        if cycle == "day":
+            self.rect = self.image_day.get_rect()
+        else:
+            self.rect = self.image_night.get_rect()
+        self.rect.y = HEIGHT//3 * 2
+        self.rect.x = self.rect.y//10
     def draw(self, win):
-        win.blit(self.image, (self.rect.x, self.rect.y))
+        if cycle == "day":
+            win.blit(self.image_day, (self.rect.x, self.rect.y))
+        else:
+            win.blit(self.image_night, (self.rect.x, self.rect.y))
     def move(self, change):
         # if the players y is close enough to the ground then he will be placed on the ground
         if self.rect.y > 390 and self.rect.y < 400:
@@ -147,41 +177,63 @@ class Dinosaur(pygame.sprite.Sprite):
 class Cactus(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = random.choice(cactuses_images)
+        # getting a random cactus image
+        rand = random.randint(0,len(cactuses_images_day)-1)
+        self.image_day = cactuses_images_day[rand]
+        self.image_night = cactuses_images_night[rand]
         self.size = random.choice(cactus_sizes)
-        self.image = pygame.transform.scale(self.image, (self.size, self.size))
-        self.rect = self.image.get_rect()
-        self.rect.x = 800
-        self.rect.y = 500 - self.image.get_height()
+        self.image_day = pygame.transform.scale(self.image_day, (self.size, self.size))
+        self.image_night = pygame.transform.scale(self.image_night, (self.size, self.size))
+        self.rect = self.image_day.get_rect()
+        if cycle == "day":
+            self.image = self.image_day
+        else:
+            self.image = self.image_night
+        self.rect.x = WIDTH
+        self.rect.y = 500 - self.image_day.get_height()
         self.type = "cactus"
-    def draw(self, win):
-        win.blit(self.image, (self.rect.x, self.rect.y))
     def move(self):
         self.rect.x -= 10
 
 class Pterodactyl(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = resized_pterodactyl
+        self.image_day = resized_pterodactyl_day
+        self.image_night = resized_pterodactyl_night
+        if cycle == "day":
+            self.image = self.image_day
+        else:
+            self.image = self.image_night
         self.rect = self.image.get_rect()
-        self.rect.x = 800
+        self.rect.x = WIDTH
         self.rect.y = random.choice(pterodactyl_heights)
         self.type = "pterodactyl"
-    def draw(self, win):
-        win.blit(self.image, (self.rect.x, self.rect.y))
     def move(self):
         self.rect.x -= 10
 
 # Initializing the game
 pygame.init()
 
+# Setting up the screen
+WIDTH, HEIGHT = 800, 600
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Dino Game")
+icon = pygame.image.load('dinosaur1.png')
+pygame.display.set_icon(icon)
+
 # setting up images of dinosaur and the cactuses
-dinosaur = pygame.image.load('dinosaur1.png')
-resized_dinosaur = pygame.transform.scale(dinosaur, (100, 100))
+dinosaur_day = pygame.image.load('dinosaur1.png')
+resized_dinosaur_day = pygame.transform.scale(dinosaur_day, (100, 100))
+dinosaur_night = pygame.image.load('dinosaur2.png')
+resized_dinosaur_night = pygame.transform.scale(dinosaur_night, (100, 100))
+
+# cycle has to be defined pretty high, because its used in the classes
+cycle = "day"
 
 # creating a sprite group for the enemies
 enemies = pygame.sprite.Group()
 
+# these are for day
 cactus_sizes = [70,75,80]
 cactus1 = pygame.image.load('cactus1.png')
 cactus2 = pygame.image.load('cactus2.png')
@@ -193,17 +245,35 @@ cactus7 = pygame.image.load('cactus7.png')
 cactus8 = pygame.image.load('cactus8.png')
 cactus9 = pygame.image.load('cactus9.png')
 cactus10 = pygame.image.load('cactus10.png')
-cactuses_images = [cactus1, cactus2, cactus3, cactus4, cactus5, cactus6, cactus7, cactus8, cactus9, cactus10]
+cactuses_images_day = [cactus1, cactus2, cactus3, cactus4, cactus5, cactus6, cactus7, cactus8, cactus9, cactus10]
 
-pterodactyl = pygame.image.load('pterodactyl_temporary.png')
-resized_pterodactyl = pygame.transform.scale(pterodactyl, (70, 70))
-chance_of_pterodactyl = 5
+# these are for night
+cactus1_night = pygame.image.load('cactus1_night.png')
+cactus2_night = pygame.image.load('cactus2_night.png')
+cactus3_night = pygame.image.load('cactus3_night.png')
+cactus4_night = pygame.image.load('cactus4_night.png')
+cactus5_night = pygame.image.load('cactus5_night.png')
+cactus6_night = pygame.image.load('cactus6_night.png')
+cactus7_night = pygame.image.load('cactus7_night.png')
+cactus8_night = pygame.image.load('cactus8_night.png')
+cactus9_night = pygame.image.load('cactus9_night.png')
+cactus10_night = pygame.image.load('cactus10_night.png')
+cactuses_images_night = [cactus1_night, cactus2_night, cactus3_night, cactus4_night, cactus5_night, cactus6_night, cactus7_night, cactus8_night, cactus9_night, cactus10_night]
+
+pterodactyl_day = pygame.image.load('pterodactyl_temporary.png')
+resized_pterodactyl_day = pygame.transform.scale(pterodactyl_day, (70, 70))
+pterodactyl_night = pygame.image.load('pterodactyl_temporary2.png')
+resized_pterodactyl_night = pygame.transform.scale(pterodactyl_night, (70, 70))
+
+chance_of_pterodactyl = 20
 pterodactyl_spawn = False
-pterodactyl_heights = [400-resized_pterodactyl.get_height(), 450-resized_pterodactyl.get_height(), 500-resized_pterodactyl.get_height()]
+pterodactyl_heights = [400-resized_pterodactyl_day.get_height(), 450-resized_pterodactyl_day.get_height(), 500-resized_pterodactyl_day.get_height()]
 pterodactyls = pygame.sprite.Group()
 
 settings_button = pygame.image.load('settings.png')
 resized_settings_button = pygame.transform.scale(settings_button, (50, 50))
+settings_button_night = pygame.image.load('settings_night.png')
+resized_settings_button_night = pygame.transform.scale(settings_button_night, (50, 50))
 
 # setting up a font for the texts
 font = pygame.font.Font(None, 36)
@@ -217,12 +287,6 @@ player_change = 0
 cactus = Cactus()
 enemies.add(cactus)
 
-# Setting up the screen
-WIDTH, HEIGHT = 800, 600
-win = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Dino Game")
-pygame.display.set_icon(resized_dinosaur)
-
 # Setting up the clock
 clock = pygame.time.Clock()
 
@@ -234,9 +298,8 @@ time = [0,0]
 running = True
 game_over = False
 settings = False
-day_night_cycle = False # I will use this later
+day_night_cycle = True
 switch_cycle = False
-cycle = "day"
 while running:
 
     # starting the timer thread
@@ -249,9 +312,9 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE or event.key == pygame.K_UP or event.key == pygame.K_w:
-                player_change = -resized_dinosaur.get_height()*1.8
+                player_change = -resized_dinosaur_day.get_height()*1.8
             if event.key == pygame.K_DOWN or event.key == pygame.K_s:
-                player_change = resized_dinosaur.get_height()//2
+                player_change = resized_dinosaur_day.get_height()//2
             if event.key == pygame.K_r:
                 reset()
         if event.type == pygame.KEYUP:
@@ -261,7 +324,7 @@ while running:
             if event.key == pygame.K_UP or event.key == pygame.K_w or event.key == pygame.K_SPACE:
                 player_change = 0
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if 750 <= event.pos[0] <= 800 and 10 <= event.pos[1] <= 60:
+            if 750 <= event.pos[0] <= WIDTH and 10 <= event.pos[1] <= 60:
                 settings = True
                 settings_window()
     
@@ -269,16 +332,31 @@ while running:
     player.move(player_change)
 
     # background
-    win.fill((255, 255, 255))
+    if cycle == "day":
+        win.fill((255, 255, 255))
+    else:
+        win.fill((0, 0, 0))
     # drawing a straight line to represent the ground
-    pygame.draw.line(win, (0, 0, 0), (0, 500), (800, 500), 5)
+    if cycle == "day":
+        pygame.draw.line(win, (0, 0, 0), (0, 500), (800, 500), 5)
+    else:
+        pygame.draw.line(win, (255, 255, 255), (0, 500), (800, 500), 5)
     # drawing score in the top left corner
-    text = font.render("Score: "+str(score), True, (0, 0, 0))
+    if cycle == "day":
+        text = font.render("Score: "+str(score), True, (0, 0, 0))
+    else:
+        text = font.render("Score: "+str(score), True, (255, 255, 255))
     win.blit(text, (10, 10))
     # drawing the settings button in the top right corner
-    win.blit(resized_settings_button, (750, 10))
+    if cycle == "day":
+        win.blit(resized_settings_button, (750, 10))
+    else:
+        win.blit(resized_settings_button_night, (750, 10))
     # drawing the time in the top middle of the screen
-    text = font.render(str(time[0])+"m "+str(time[1]) + "s", True, (0, 0, 0))
+    if cycle == "day":
+        text = font.render(str(time[0])+"m "+str(time[1]) + "s", True, (0, 0, 0))
+    else:
+        text = font.render(str(time[0])+"m "+str(time[1]) + "s", True, (255, 255, 255))
     win.blit(text, (350, 10))
 
     # Drawing the player
@@ -304,13 +382,18 @@ while running:
                     enemies.add(cactus)
             if enemy.type == "pterodactyl":
                 enemies.remove(enemy)
-                pterodactyl = Pterodactyl()
-                enemies.add(pterodactyl)
-                pterodactyls.add(pterodactyl)
+                if pterodactyl_spawn:
+                    pterodactyl = Pterodactyl()
+                    enemies.add(pterodactyl)
+                    pterodactyls.add(pterodactyl)
+                    pterodactyl_spawn = False
+                else:
+                    cactus = Cactus()
+                    enemies.add(cactus)
             score += 1
 
     # spawning the pterodactyl
-    if score > 0 and len(pterodactyls) == 0:
+    if score > 20 and len(pterodactyls) == 0:
         rand = random.randint(0, 100)
         if rand < chance_of_pterodactyl:
             pterodactyl_spawn = True
@@ -320,7 +403,7 @@ while running:
         game_over_screen()
 
     # this is the day night cycle switching, it will be used later when I add the opposite color images
-    if time[1] == 59:
+    if time[1] == 59 and day_night_cycle:
         switch_cycle = True
     
     # if the code reaches this part then the settings window is closed
@@ -328,7 +411,7 @@ while running:
 
     # Updating the display
     pygame.display.update()
-    
+
     # Frame rate
     clock.tick(60)
 
